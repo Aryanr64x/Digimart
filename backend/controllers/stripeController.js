2fb83ff4db758e713d9ce15116df8726d7305ebd
saket <aryansaket@yahoo.com>
2023-01-14 14:08:08 +0530

    Stripe Link Modifications

--- backend/controllers/stripeController.js
+++ backend/controllers/stripeController.js
@@ -46,8 +46,8 @@ export const pay = asyncHandler(async(req, res)=>{
         const session  = await s.checkout.sessions.create({
             line_items: lineItems,
             mode: 'payment',
-            success_url: "http://localhost:3000/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: 'http://localhost:3000/cancel'
+            success_url: "https://www.digimartt.netlify.app/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url: 'http://www.digimartt.netlify.app/cancel'
         })
         res.json({
             url: session.url
